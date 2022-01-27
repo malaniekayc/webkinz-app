8d3d997d79eff67deba8613bd053190ddf397888
Malanie Cline <mkc0032@mix.wvu.edu.com>
2022-01-26 23:56:57 -0500

    Added top and social nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TopNavBarComponent } from './navigation/topNavBar.component';
+import { SocialNavBarComponent } from './navigation/socialNavBar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavBarComponent,
+    SocialNavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
